25b67d7aad15a2c57c3507a3b8f8c09beec0a1f2
Amit Choudhary <amitchoudhary0523@gmail.com>
2025-03-09 12:19:08 +0530

    Updated test_generic_doubly_linked_list_library_as_map.c
    Updated test_generic_doubly_linked_list_library_as_map.c

--- test_generic_doubly_linked_list_library_as_map.c
+++ test_generic_doubly_linked_list_library_as_map.c
@@ -1,13 +1,18 @@
 
 /*
+ * License:
+ *
+ * This file has been released under "unlicense" license
+ * (https://unlicense.org).
+ *
  * This is free and unencumbered software released into the public domain.
  *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
+ * Anyone is free to copy, modify, publish, use, compile, sell, or distribute
+ * this software, either in source code form or as a compiled binary, for any
+ * purpose, commercial or non-commercial, and by any means.
  *
- * For more information, please refer to <https://unlicense.org/>
+ * For more information about this license, please visit
+ * <https://unlicense.org>.
  */
 
 /*
